03aff97e9b651eb604caeb1300104b286a30c791
lorenzo <l.romagnoli@arduino.cc>
2016-07-11 10:41:52 +0200

    added empty auth file

---
+++ auth.js
@@ -0,0 +1,15 @@
+var auth = {
+    //TelegramBotToken
+    'TelegramBotToken': 'xxxxxxxxxxx',
+
+    //wit access token
+    'accessToken': 'xxxxxxxxxxx',
+
+    //arduino Cloud
+    'name': "xxxxxxxxxxx",
+    'thing_ID': "xxxxxxxxxxx",
+    'thing_PSW': "xxxxxxxxxxx",
+    'username': "xxxxxxxxxxx"
+}
+
+module.exports = auth;
